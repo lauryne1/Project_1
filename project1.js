d4e594ab79dbf9f4e8bd8a92308241a5f41fa0e0
laurynejd <laurynejosiasdady@gmail.com>
2022-12-11 02:29:26 +0100

    mycommit

---
+++ project1.js
@@ -0,0 +1,8 @@
+function mouseOver(element) {
+  element.style.color = "blue";
+  element.innerHTML = "Hello Lauryne";
+}
+function mouseOut(element) {
+  element.style.color = "black ";
+  element.innerHTML = "Hello World!";
+}
